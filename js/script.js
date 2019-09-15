2225136e3457c8763c3db1576fc9571047b33487
Veronika Šilhová <eaavsi@students.eaaa.dk>
2019-09-15 18:31:23 +0200

    add some comments

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ let activePage = "welcome";
 // initialize the plugin navbar
 const tabs = document.querySelector("#tabs")
 const tabsInstance = M.Tabs.init(tabs, {
-  onShow: function(sectionElement) {
+  onShow: function (sectionElement) {
     // console.log(sectionElement.id);
 
     location.href = `#${sectionElement.id}`;
@@ -14,24 +14,13 @@ const tabsInstance = M.Tabs.init(tabs, {
 });
 
 // initialize the floating button
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   let elems = document.querySelectorAll('.fixed-action-btn');
   let instances = M.FloatingActionButton.init(elems, {
     direction: 'left',
     hoverEnabled: false
   });
 });
-
-function showLoader(show) {
-  let loader = document.querySelector('#cooking');
-  if (show) {
-    loader.classList.remove("hide");
-
-  } else {
-    loader.classList.add("hide");
-
-  }
-}
 
 // hide all pages
 function hideAllPages() {
@@ -41,6 +30,17 @@ function hideAllPages() {
   }
 };
 
+function showLoader(show) {
+  let loader = document.querySelector('#cooking');
+  if (show) {
+    loader.classList.remove("hide");
+    // hideAllPages(pageId, isTab);
+  } else {
+    loader.classList.add("hide");
+
+  }
+}
+
 // show page or tab
 function showPage(pageId, isTab) {
   let display = 'block';
@@ -48,7 +48,7 @@ function showPage(pageId, isTab) {
   activePage = pageId;
   hideAllPages();
 
-  // hide navbar if welcome or login page are active
+  // hide navbar if welcome or login pages are active
   if (activePage === 'welcome' || activePage === 'login') {
     display = 'flex';
 
@@ -57,13 +57,13 @@ function showPage(pageId, isTab) {
     document.querySelector(".nav-extended").classList.remove("hide-navbar")
   };
 
-
-  if (activePage === 'favourites' || activePage === 'recipes') {
-    console.log("you are shit");
-    showPage("login");
-  } else {
-    console.log("you are not clicking on favourite or recipes page");
-  };
+  // // redirect to log in page if user is not logged in
+  // if (activePage === 'favourites' || activePage === 'recipes') {
+  //   console.log("you are shit");
+  //   showPage("login");
+  // } else {
+  //   console.log("you are not clicking on favourite or recipes page");
+  // };
 
   document.querySelector(`#${pageId}`).style.display = display;
   setActiveTab(pageId);
@@ -71,11 +71,12 @@ function showPage(pageId, isTab) {
   if (isTab) {
     tabsInstance.select(pageId);
   }
-  setTimeout(function() {
+  setTimeout(function () {
     showLoader(false);
-  }, 10000);
+  }, 2000);
 
 };
+
 
 // sets active tabbar/ menu item
 function setActiveTab(pageId) {
@@ -129,16 +130,17 @@ const myRecipesRef = db.collection("myRecipes");
 let allRecipes = {};
 let allIngredients = {};
 let myRecipes = {};
+let userData = null;
 
 // watch the database ref for changes
-recipeRef.onSnapshot(function(snapshotData) {
+recipeRef.onSnapshot(function (snapshotData) {
   let recipes = snapshotData.docs;
   allRecipes = recipes;
   // console.log(snapshotData);
   appendRecipes(recipes);
 });
 // ingredients
-ingredientRef.onSnapshot(function(snapshotData) {
+ingredientRef.onSnapshot(function (snapshotData) {
   let ingredients = snapshotData.docs;
   allIngredients = ingredients;
   // console.log(allIngredients);
@@ -169,18 +171,22 @@ const uiConfig = {
 const ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 // Listen on authentication state change
-firebase.auth().onAuthStateChanged(function(user) {
-  let userData = user;
-  setTimeout(function() {
+firebase.auth().onAuthStateChanged(function (user) {
+  userData = user;
+  setTimeout(function () {
     showLoader(false);
   }, 5000);
 
-  // let tabbar = document.querySelector('#tabbar');
   if (user) { // if user exists and is authenticated
     // appendMyRecipes(myRecipes);
 
     setDefaultPage('search');
-    console.log(userData.photoURL);
+
+    // hide log in button and show profile floating button
+    document.getElementById('profile-photo-button').style.display = 'block';
+    document.getElementById('navbar-login').style.display = 'none';
+
+    // Show profile photo
     let htmlTemplate = "";
     htmlTemplate = `
     <img id="user-image" class="profile-photo" src="${userData.photoURL}" alt="Profile photo">
@@ -188,53 +194,46 @@ firebase.auth().onAuthStateChanged(function(user) {
     document.querySelector('.add-profile-photo').innerHTML = htmlTemplate;
 
     console.log("user is log in");
-    // console.log(user);
 
     favouritesRef.where("userId", "==", user.uid)
       .get()
-      .then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
+      .then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
           console.log(doc.data());
         });
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log("Error getting favourites: ", error);
       });
-    document.getElementById('profile-photo-button').style.display = 'block';
-    document.getElementById('navbar-login').style.display = 'none';
 
-    //  tabbar.classList.remove("hide");
   } else { // if user is not logged in
-    //   console.log("im log out")
+    // hide profile floating button and show log in button
     document.getElementById('profile-photo-button').style.display = 'none';
     document.getElementById('navbar-login').style.display = 'flex';
-    //   if (activePage === "favourites" || activePage === "recipes") {
-    //     console.log("im log out on fav / recipes")
-    //     showPage("login");
-    //   } else {
-    //     console.log("im log out")
-    //   }
+
+    // if (activePage === "favourites" || activePage === "recipes") {
+    //   console.log("im log out on fav / recipes")
+    //   showPage("login");
+    // } else {
+    //   console.log("im log out")
+    // }
+
     ui.start('#firebaseui-auth-container', uiConfig);
   }
 });
 let provider = new firebase.auth.FacebookAuthProvider();
 provider.addScope('email, picture');
-// console.log(provider);
-
 
 // sign out user
 function logout() {
-
   //firebase.auth().signOut();
-  firebase.auth().signOut().then(function() {
-
+  firebase.auth().signOut().then(function () {
     // Sign-out successful.
     console.log("Succes sign out");
     M.toast({
-      html: 'Successfuly Log out'
+      html: 'Successfuly logged out.'
     })
-  }).catch(function(error) {
-
+  }).catch(function (error) {
     // An error happened.
     console.log("Error sign out");
   });
@@ -242,7 +241,6 @@ function logout() {
 
 // RECIPES
 function appendRecipes(recipes) {
-
   let htmlTemplate = "";
   for (let recipe of recipes) {
     htmlTemplate += `
@@ -264,19 +262,17 @@ function appendRecipes(recipes) {
     `;
   }
   document.querySelector('#recipes-container').innerHTML = htmlTemplate;
-  // console.log(recipes);
 }
 
 function openRecipe(id) {
   const recipe = allRecipes.find(r => r.id === id);
-  // console.log(recipe);
   let htmlTemplate = "";
-
+  // loop throught all steps
   let steps = recipe.data().steps.map(step => {
-    // console.log(step);
     return `<p class="recipe-step">${step}</p>`
   })
 
+  // loop throught all ingredients
   let recipeIngredients = recipe.data().ingredients.map(recipeIngredient => {
     // console.log(recipeIngredient.title);
     return `<li class="recipe-ingredient">${recipeIngredient.amount + " " + recipeIngredient.title}</li>`
@@ -288,7 +284,7 @@ function openRecipe(id) {
     <div class="card-image" id="recipe-card-img">
       <img src="${recipe.data().img}">
       <p class="time">${recipe.data().time}'</p>
-      <a class="btn-floating halfway-fab waves-effect waves-light recipe-floating-button" onclick="favourite()"><i
+      <a class="btn-floating halfway-fab waves-effect waves-light recipe-floating-button" onclick="favourite('${recipe.id}')"><i
           class="material-icons fav">favorite</i></a>
     </div>
     <h2>Ingredients</h2>
@@ -303,50 +299,91 @@ function openRecipe(id) {
   showPage("recipe");
 }
 
-// favourite
+// add or remove recipe from favourites
 function favourite(id) {
   const recipeElement = document.querySelector(`#recipe-${id} .fav`);
 
-
+  // change colour of the add favourite recipes button
   recipeElement.style.color = "red";
   recipeElement.style.background = "white";
-  // console.log(id);
 
-  const recipe = allRecipes.find(r => r.id === id);
-  let htmlTemplate = "";
+  // console.log(userData.uid);
+  // console.log(`recipes/${id}`);
 
-  for (let recipe of recipes) {
-    htmlTemplate += `
-      <div class="col s12 m6 l4">
-        <div class="card">
-          <div class="card-image">
-            <img src="${recipe.data().img}">
-            <a class="btn-floating halfway-fab waves-effect waves-light " onclick="favourite();">
-              <i class="material-icons fav">favorite</i>
-            </a>
-          </div>
-          <div class="card-content">
-          <p class="time">${recipe.data().time}'</p>
-            <span class="card-title">${recipe.data().title}</span>
-            <a class="waves-effect waves-light btn radius" onclick="openRecipe('${recipe.id}')">OPEN RECIPE</a>
-          </div>
-        </div>
-      </div>
-    `;
-  }
-  document.querySelector('#recipes-container').innerHTML = htmlTemplate;
-  console.log(recipes);
+  // favouritesRef.where("recipe", "==", `recipes/${id}`)
+  //   .get()
+  //   .then(function (querySnapshot) {
+  //     console.log("snapshot", querySnapshot);
+
+  //     querySnapshot.forEach(function (doc) {
+  //       console.log(doc.data());
+  //     });
+  //   })
+  //   .catch(function (error) {
+  //     console.log("Error getting favourites: ", error);
+  //   });
+
+  // find rcipe by id
+  // const recipe = allRecipes.find(r => r.id === id);
+
+
+
+  // check if recipe is in favourites https://stackoverflow.com/questions/37910008/check-if-value-exists-in-firebase-db
+  //favouritesRef.orderByChild
+  // firebase.database().ref("favourites").orderByChild("recipe").equalTo(recipe).once("value", snapshot => {
+  //   if (snapshot.exists()) {
+  //     const userData = snapshot.val();
+  //     console.log("exists!", userData);
+
+
+  //   }
+  // });
+
+  // favouritesRef.child(recipe).equalTo("recipe").once('value', function (snapshot) {
+  //   if (snapshot.exists()) {
+  //     alert('exists');
+  //   } else {
+  //     alert('nope');
+  //   }
+  // });
+
+
+
+
+
+  // let htmlTemplate = "";
+
+  // for (let recipe of recipes) {
+  //   htmlTemplate += `
+  //     <div class="col s12 m6 l4">
+  //       <div class="card">
+  //         <div class="card-image">
+  //           <img src="${recipe.data().img}">
+  //           <a class="btn-floating halfway-fab waves-effect waves-light " onclick="favourite();">
+  //             <i class="material-icons fav">favorite</i>
+  //           </a>
+  //         </div>
+  //         <div class="card-content">
+  //         <p class="time">${recipe.data().time}'</p>
+  //           <span class="card-title">${recipe.data().title}</span>
+  //           <a class="waves-effect waves-light btn radius" onclick="openRecipe('${recipe.id}')">OPEN RECIPE</a>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   `;
+  // }
+  // document.querySelector('#recipes-container').innerHTML = htmlTemplate;
+  // console.log(recipes);
 }
 
 // search-autocomplete function
-
 // Getting the ingridients from the collection of firebase
-db.collection("ingredients").get().then(function(querySnapshot) {
+db.collection("ingredients").get().then(function (querySnapshot) {
   //Convert the objects to display them on the search
   let data = {};
   let i = 0;
   //Function to get the data(ingridients) to use them in the search
-  querySnapshot.forEach(function(doc) {
+  querySnapshot.forEach(function (doc) {
     data[`${doc.data().name}`] = null;
     i++;
   });
@@ -355,7 +392,7 @@ db.collection("ingredients").get().then(function(querySnapshot) {
   //let to display the data, in this case, our ingredients and append them to the DOM
   let options = {
     data: data,
-    onAutocomplete: function(doc) {
+    onAutocomplete: function (doc) {
       console.log(doc);
       let htmlTemplate = "";
       htmlTemplate = `
